6e949ce2b8ba6bb38b9d16167fb963b2c1d15be1
Artis84 <lopsta10@gmail.com>
2019-02-14 10:22:59 +0100

    Fix Rooting

---
+++ vue_dqjsn/src/components/Register.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Register</h1>
+  </div>
+</template>
+
+<script>
+console.log('test')
+export default {
+  data() {
+    return {
+      quote: 'test'
+    }
+  }
+}
+</script>
+
+<style scoped></style>
